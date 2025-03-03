7d82cff628401f37ea52761ca6a307fcc45297ae
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2025-03-02 23:22:05 -0500

    seems to be working

--- backend/db/dbConfig.js
+++ backend/db/dbConfig.js
@@ -10,20 +10,20 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const pgp = pgPromise();
 
-// const cn = process.env.DATABASE_URL || {
-//   user: process.env.PG_USER || "fsmathias",
-//   host: process.env.PG_HOST || "localhost",
-//   database: process.env.PG_NAME || "travel_recommendation",
-//   port: process.env.PG_PORT || 5432, // Default PostgreSQL port
-//   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // Required for Supabase
-// };
-
-const cn = 
-  {user: process.env.PG_USER || "fsmathias",
+const cn = process.env.DATABASE_URL || {
+  user: process.env.PG_USER || "fsmathias",
   host: process.env.PG_HOST || "localhost",
   database: process.env.PG_NAME || "travel_recommendation",
   port: process.env.PG_PORT || 5432, // Default PostgreSQL port
-  }
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // Required for Supabase
+};
+
+// const cn = 
+//   {user: process.env.PG_USER || "fsmathias",
+//   host: process.env.PG_HOST || "localhost",
+//   database: process.env.PG_NAME || "travel_recommendation",
+//   port: process.env.PG_PORT || 5432, // Default PostgreSQL port
+//   }
   
 
 
