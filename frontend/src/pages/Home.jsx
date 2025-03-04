6db901fda573c0a636d90e5ce210e53e25cf48f7
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2025-03-03 21:26:19 -0500

    adding a break element

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
       <div>
         <h1>Welcome to the Travel Recommender</h1>
         <p>Discover personalized travel recommendations based on your preferences.</p>
-        <a href="/profile">Go to Profile</a>
+        <a href="/profile">Go to Profile</a><br />
         <a href="/testing">Testing FrontEnd-BackEnd connection</a>
       </div>
     );
