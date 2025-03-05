7ddd3062375649f0fa80ba9be07da06d174f6615
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2025-03-04 22:18:22 -0500

    backgroundSlider got better

--- frontend/src/pages/BackgroundSlider.jsx
+++ frontend/src/pages/BackgroundSlider.jsx
@@ -1,8 +1,44 @@
-import React from "react";
-import "../styles/BackgroundSlider.css"; // Import CSS file
+import React, { useState, useEffect } from "react";
+import "../styles/BackgroundSlider.css"; // Keep your CSS for general styling
+
+const images = [
+  "/vacationImages/pexels-alexmoliski-28969898.jpg",
+  "/vacationImages/pexels-te-lensfix-380994-1371360.jpg",
+  "/vacationImages/pexels-asadphoto-1268855.jpg",
+  "/vacationImages/pexels-recalmedia-60217.jpg",
+  "/vacationImages/pexels-riciardus-307008.jpg",
+  "/vacationImages/pexels-pixabay-208701.jpg",
+  "/vacationImages/pexels-jimmy-teoh-294331-1010657.jpg",
+  "/vacationImages/pexels-asadphoto-1024989.jpg",
+  "/vacationImages/pexels-pixabay-38238.jpg",
+  "/vacationImages/pexels-freestockpro-1008155.jpg",
+  "/vacationImages/pexels-asadphoto-457882.jpg",
+  "/vacationImages/pexels-mohamed-hussain-629997-30951335.jpg",
+  "/vacationImages/pexels-fmaderebner-238622.jpg",
+  "/vacationImages/pexels-tatianasyrikova-3934023.jpg"
+  
+  
+];
 
 const BackgroundSlider = () => {
-  return <div className="background-slider"></div>;
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 5000); // Change every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  return (
+    <div
+      className="background-slider"
+      style={{
+        backgroundImage: `url(${images[currentImage]})`,
+      }}
+    ></div>
+  );
 };
 
 export default BackgroundSlider;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import BackgroundSlider from "./BackgroundSlider.jsx"
+import ImagesSlider from "./ImagesSlider.jsx"
+
 
 const Home = () => {
     return (
       <div>
-        {/* <BackgroundSlider /> */}
+        <BackgroundSlider />
         <h1>Welcome to the Travel Recommender</h1>
         <p>Discover personalized travel recommendations based on your preferences.</p>
         <a href="/profile">Go to Profile</a><br />
         <a href="/testing">Testing FrontEnd-BackEnd connection</a>
+        {/* <ImagesSlider /> */}
       </div>
     );
   };
