2fd18a8c7a76df5cf53bf1d8e80a3342a42ed91f
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2025-03-04 21:40:07 -0500

    BackgroundSlider.jsx workingish

---
+++ frontend/src/pages/BackgroundSlider.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/BackgroundSlider.css"; // Import CSS file
+
+const BackgroundSlider = () => {
+  return <div className="background-slider"></div>;
+};
+
+export default BackgroundSlider;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import BackgroundSlider from "./BackgroundSlider.jsx"
+
 const Home = () => {
     return (
       <div>
+        {/* <BackgroundSlider /> */}
         <h1>Welcome to the Travel Recommender</h1>
         <p>Discover personalized travel recommendations based on your preferences.</p>
         <a href="/profile">Go to Profile</a><br />
