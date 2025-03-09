71e2881450af78a83e1b402bd8168fb54d1b133a
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2025-03-09 13:58:07 -0400

    changing the icon in the navbar

--- frontend/src/pages/TravelNavbar.jsx
+++ frontend/src/pages/TravelNavbar.jsx
@@ -7,7 +7,16 @@ const TravelNavbar = () => {
       <Navbar expand="lg" className="custom-navbar">
         <Container fluid className="px-4">
           {/* Brand Logo */}
-          <Navbar.Brand href="#" className="brand-logo">🌍 A I &nbsp; Travels</Navbar.Brand>
+          <Navbar.Brand 
+            href="/" 
+            className="brand-logo" 
+            style={{display: "flex", alignItems: "center"}}>
+                <img 
+                    src="/favicon.ico"
+                    alt="logo" 
+                    style={{ width: "24px", height: "24px", marginRight: "8px" }} 
+                    /> 
+                    <span style={{ lineHeight: "1" }}>A I &nbsp; Travels</span></Navbar.Brand>
   
           {/* Navbar Toggle Button */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
